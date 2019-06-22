bae5956384d367b6b7f24c5838060574dcb906b5
mikeingley <michael.ingley@gmail.com>
2019-06-22 14:46:34 -0700

    updated code

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-// // write your code here
-// http => require('http');
-// port => '8080';
-// server => http.createServer(function (req, res){
-//     //log msg when request recieved
-//     console.log('Recieved ' + req.method + ' request for: ' + req.url)
-//     //send headers
-//     res.setHeader(200, {'Content-Type': 'text/plain'});
-
-//     //send body response
-//     res.end('Hello World');
-// });
-
-// server.listen(8080, 'localhost', null, function(){
-//     //log msg that server is listening and port 
-//     console.log('Server is listening on localhost:8080');
-// });
-
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 8080;
+http => require('http');
+port => 8080;
 
 const server = http.createServer((req, res) => {
   console.log('Recieved ' + req.method + ' request for: ' + req.url)
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
